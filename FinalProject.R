168d222841b7691bc7244e9c28f42c9bd62ae8f8
Sage Wentzell-Brehme <swentzel@wellesley.edu>
2020-12-10 00:14:17 -0500

    ^^

--- FinalProject.R
+++ FinalProject.R
@@ -10,13 +10,14 @@ library(tidyr)
 # set working directory
 setwd("~/Wellesley/Year 4/Bisc_307/Final/BISC_ES_307_Final")
 
+
+#Hubbard Brook Data
 HB_temp <- read.csv("data/HBEF_air_temp_daily_1957-2019.csv")
-
-
-HB_frost <- read.txt("data/HBEF_frost_1956-2015.txt")
 HB_snowcourse <- read.csv("data/HBEF_snowcourse_1956-2020.csv")
-HB_snowdepth <- read.csv("data/HBEFsnowDepth_1956-2019.csv")
 
+HB_snowdepth <- read.csv("data/HBEFsnowDepth_1956-2019.csv") # don't really need to include this
+
+#HF data?
 HF_snow <- read.csv("data/hf237-01-snow-depth.csv")
 HF_temp <- read.csv("data/hf300-05-daily-temp-m.csv")
 
